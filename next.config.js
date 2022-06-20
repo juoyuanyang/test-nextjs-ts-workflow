858d96649df514ad0f7b6690d9bb547ee7c877a5
Joe Yang <joe_yang@morrisonexpress.com>
2022-06-20 16:18:36 +0800

    Finalize deployment flow

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production';
-const basePathPrefix = (isProd
-  ? 'https://raw.githubusercontent.com/juoyuanyang/test-nextjs-ts-workflow/deployment/out/'
-  : ''
-);
-
 module.exports = {
   reactStrictMode: true,
   distDir: 'dist',
   images: {
     loader: 'akamai',
-    path: basePathPrefix,
+    path: './',
   },
-  // assetPrefix: './',
-  assetPrefix: basePathPrefix,
+  assetPrefix: './',
 };
