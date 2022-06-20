f1935d726b5648dcf1fe2ae1cad0e0d23468f5c2
Joe Yang <joe_yang@morrisonexpress.com>
2022-06-20 15:42:31 +0800

    Fix base URI path

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+const basePathPrefix = 'https://raw.githubusercontent.com/juoyuanyang/test-nextjs-ts-workflow/deployment/out/';
+
 module.exports = {
   reactStrictMode: true,
   distDir: 'dist',
-  // images: {
-  //   loader: 'akamai',
-  //   path: '.',
-  // },
+  images: {
+    loader: 'akamai',
+    path: basePathPrefix,
+  },
   // assetPrefix: './',
-  assetPrefix: 'https://raw.githubusercontent.com/juoyuanyang/test-nextjs-ts-workflow/deployment/out/',
+  assetPrefix: basePathPrefix,
 };
