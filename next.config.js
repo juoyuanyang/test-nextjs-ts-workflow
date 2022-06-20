5f9e3747075f040c797a8811a5fe07dbee2c741a
Joe Yang <joe_yang@morrisonexpress.com>
2022-06-20 15:57:14 +0800

    Fix deployment flow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-const basePathPrefix = 'https://raw.githubusercontent.com/juoyuanyang/test-nextjs-ts-workflow/deployment/out/';
+const isProd = process.env.NODE_ENV === 'production';
+const basePathPrefix = (isProd
+  ? 'https://raw.githubusercontent.com/juoyuanyang/test-nextjs-ts-workflow/deployment/out/'
+  : ''
+);
 
 module.exports = {
   reactStrictMode: true,
