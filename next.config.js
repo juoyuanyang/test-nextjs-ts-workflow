1769d816ffa40a54474c9af815fb338329904f1d
Joe Yang <joe_yang@morrisonexpress.com>
2022-06-20 14:53:38 +0800

    Add github action

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   distDir: 'dist',
-  images: {
-    loader: 'akamai',
-    path: '/',
-  },
+  // images: {
+  //   loader: 'akamai',
+  //   path: '.',
+  // },
+  // assetPrefix: './',
+  assetPrefix: 'https://raw.githubusercontent.com/juoyuanyang/test-nextjs-ts-workflow/deployment/out/',
 };
